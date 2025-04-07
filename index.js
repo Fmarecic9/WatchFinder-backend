675d7b485aaee0328d87dff8008c95b49287b346
Fmar69 <filip2mar@gmail.com>
2025-04-07 21:26:29 +0200

    Dodane backend rute za dodavanje i uklanjanje satova sa korisnickog profila

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import { connectToDatabase } from './db.js'
 import userRouter from './routes/users.js'
 import watchRouter from './routes/watches.js'
 import userProfRouter from './routes/userProfile.js'
@@ -12,9 +11,10 @@ app.use(express.json())
 const PORT = 3000
 app.use(cors())
 
+
 app.use('/users', userRouter)
 app.use('/watches', watchRouter)
-app.use('/userProfile', userProfRouter)
+app.use('/profile', userProfRouter)
 
 
 
--- routes/userProfile.js
+++ routes/userProfile.js
@@ -32,26 +32,84 @@ router.get('/', authMiddleware, async(req,res)=>{
 
 })
 
-router.post('/:id', authMiddleware, async(req,res)=>{
+router.post('/wishlist/:id', authMiddleware, async(req,res)=>{
+    let userId = req.user.id
     let watchId = req.params.id
 
     try{
-
+        let result = await userCollection.updateOne(
+            {_id: new ObjectId(userId)},
+            {$addToSet: {wishlist: new ObjectId(watchId)}}
+            
+        )
+        if (result.modifiedCount === 0){
+            return res.status(400).json({ message: "Watch was not added"});
+        }
+        res.status(200).json({ message: "Watch added to wishlist" });
+       
     }
     catch(e){
-        
+        console.error(`Not good: ${e}`)
     }
 })
 
+router.post('/owned/:id', authMiddleware, async(req,res)=>{
+    let userId = req.user.id
+    let watchId = req.params.id
 
+    try{
+        let result = await userCollection.updateOne(
+            {_id: new ObjectId(userId)},
+            {$addToSet: {ownedWatches: new ObjectId(watchId)}}
+            
+        )
+        if (result.modifiedCount === 0){
+            return res.status(400).json({ message: "Watch was not added"});
+        }
+        res.status(200).json({ message: "Watch added to owned watches" });
+       
+    }
+    catch(e){
+        console.error(`Not good: ${e}`)
+    }
+})
 
+router.patch('/wishlist/:id', authMiddleware, async(req,res)=>{
+    let userId = req.user.id
+    let watchId = req.params.id
 
+    try{
+        let result = await userCollection.updateOne(
+            { _id: new ObjectId(userId) },
+            { $pull: { wishlist: new ObjectId(watchId)}} 
+        );
+        if (result.modifiedCount === 0){
+            return res.status(400).json("Did not remove watch")
+        }
+        return res.status(200).json("Watch removed from wishlist")
+    }
+    catch(e){
+        console.error(`Error: ${e}`)
+    }
+})
 
+router.patch('/owned/:id', authMiddleware, async(req,res)=>{
+    let userId = req.user.id
+    let watchId = req.params.id
 
-
-
-
-
-
+    try{
+        let result = await userCollection.updateOne(
+            { _id: new ObjectId(userId) },
+            { $pull: { ownedWatches: new ObjectId(watchId)}} 
+        );
+        if (result.modifiedCount === 0){
+            return res.status(400).json("Did not remove watch")
+        }
+        return res.status(200).json("Watch removed from owned list")
+    }
+    catch(e){
+        console.error(`Error: ${e}`)
+    }
+})
 
 export default router
